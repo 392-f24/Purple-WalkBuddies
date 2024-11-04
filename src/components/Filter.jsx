fd6774adbdb60f400d3c2b991d46a4abf4ee514b
thomasinfor <thomaswang2003@gmail.com>
2024-11-04 15:47:14 -0600

    Add CI/CD

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -70,12 +70,13 @@ export default function Filter({resource, setResource}) {
               left: '50%',
               transform: 'translate(-50%, -50%)',
               width: 400,
+              maxWidth: "90%",
               bgcolor: 'background.paper',
               border: '2px solid #000',
               boxShadow: 24,
               p: 4,
             }}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
+            {/* <Typography id="modal-modal-title" variant="h6" component="h2">
                 Sort
             </Typography>
             <Chip
@@ -84,9 +85,9 @@ export default function Filter({resource, setResource}) {
                 onClick={() => toggleTag(setSorted)}
                 color={sorted ? 'primary' : 'default'}
                 sx={{ mt: 2 }}
-            />
+            /> */}
 
-            <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ mt: 4 }}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
                 Tags
             </Typography>
 
