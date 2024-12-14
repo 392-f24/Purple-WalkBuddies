a9d6c176383cc6c409340308c1d45ad8a1a6f6af
Wendy Huang <wenjinghuang0912@gmail.com>
2024-12-14 15:10:24 -0600

    Add files via upload

---
+++ src/RequestsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './RequestsPage.css'
+import Request_Page_List from "../Request_Page_List"
+
+const RequestsPage = () => {
+  return (
+    <div className="requests-page-container">
+      <h1>Your Requests Dashboard</h1>
+      <Request_Page_List />
+    </div>
+  );
+};
+
+export default RequestsPage;
